dd8cadf54d2ddf40577d48f0398fc235667d1e42
mohammadalmashreqe <mohammadalmashreqe@yahoo.com>
2019-04-01 14:01:44 +0300

    show snack bar whene user connected and leave

--- index.js
+++ index.js
@@ -2,13 +2,37 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var userCount=0; 
+var $ipsConnected = [];
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-   userCount++;
-    io.emit("user count",userCount);
+   
+    socket.on("user name",function(name){
+        socket.username=name;
+        incuser();
+      
+    }
+    )
+    
+  function incuser (){
+        userCount++;
+        console.log(socket.username +"  connected");
+       
+        io.emit("new user",socket.username);
+}
+    
+  
+  
+    socket.on('disconnect', function() {
+   
+
+      io.emit("leave user",socket.username);
+
+});
+   
+    
     socket.on('chat message', function(msg){
         
       io.emit('chat message',socket.username+":"+ msg);
@@ -16,23 +40,15 @@ io.on('connection', function(socket){
     });
 
 
+ 
 
 
 
-    socket.on("user name",function(name){
-        socket.username=name;
-      
-    }
-    )
 
 
   });
   //-------------------------
-    io.on('disconnect',function()
-    {
-        userCount--;
-    io.emit("user count",userCount);
-    });
+   
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
