d859608411578fa104444cc9c819f125f8593600
mohammadalmashreqe <mohammadalmashreqe@yahoo.com>
2019-04-03 10:45:18 +0300

    take input un html tag rather than window.prompt

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var cookieParser=require("cookie-parser");
+var cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
-app.get('/', function(req, res){ 
-    
+app.get('/', function (req, res) {
+
 
 
   res.sendFile(__dirname + '/index.html');
@@ -13,49 +13,47 @@ app.get('/', function(req, res){
 
 });
 
-io.on('connection', function(socket){
- 
-    
-  
-       
-
-    socket.on("user name",function(name){
-        socket.username=name;
-      
-   
-      
-    }
-    );
-
-
-    
-    
-  
-  
-    socket.on('disconnect', function() {
-   
-
-      io.emit("leave user",socket.username);
-
-});
-   
-    
-    socket.on('chat message', function(msg){
-        
-      io.emit('chat message',socket.username+":"+ msg);
-
-    });
-
-
- 
+io.on('connection', function (socket) {
 
 
 
 
+
+  socket.on("user name", function (name) {
+    socket.username = name;
+
+  }
+  );
+
+
+
+
+
+
+  socket.on('disconnect', function () {
+
+
+    io.emit("leave user", socket.username);
 
   });
-  //-------------------------
-   
-http.listen(3000, function(){
+
+
+  socket.on('chat message', function (msg) {
+
+    io.emit('chat message', socket.username + ":" + msg);
+
+  });
+
+
+
+
+
+
+
+
+});
+//-------------------------
+
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
